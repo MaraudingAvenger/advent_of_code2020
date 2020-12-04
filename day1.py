952bdf9c82eaef32a1d02294da77d325c7453738
Luke <lechambers@gmail.com>
2020-12-03 23:12:17 -0500

    Did autopep8 format on files

--- day1.py
+++ day1.py
@@ -1,19 +1,19 @@
 nums = {int(line.strip()) for line in open('day1_1.txt').readlines()}
 
 for num in nums:
-  if (check := (2020 - num)) in nums:
-    print(num, 2020-num)
-    print("Part one answer:", num * check)
-    break
+    if (check := (2020 - num)) in nums:
+        print(num, 2020-num)
+        print("Part one answer:", num * check)
+        break
 
 found = False
 for num in nums:
-  checks = {x for x in nums if x != num}
-  for second in checks:
-    if (third := 2020 - (num + second)) in checks:
-      found = True
-      print(num, second, third)
-      print("Part two answer:", num * second * third)
-      break
-  if found:
-    break
+    checks = {x for x in nums if x != num}
+    for second in checks:
+        if (third := 2020 - (num + second)) in checks:
+            found = True
+            print(num, second, third)
+            print("Part two answer:", num * second * third)
+            break
+    if found:
+        break
--- day2.py
+++ day2.py
@@ -2,37 +2,39 @@ import re
 
 lines = [line.strip().split(":") for line in open('day2_1.txt').readlines()]
 
+
 class Rule():
-  def __init__(self, s:str) -> None:
-    patt = re.compile(r"([0-9]+)\-([0-9]+) ([a-z])")
-    groups = patt.findall(s)
-    if not groups:
-      raise IndexError(f"problem matching string {s}")
-    self.low = int(groups[0][0])
-    self.hi = int(groups[0][1])
-    self.letter = groups[0][2]
+    def __init__(self, s: str) -> None:
+        patt = re.compile(r"([0-9]+)\-([0-9]+) ([a-z])")
+        groups = patt.findall(s)
+        if not groups:
+            raise IndexError(f"problem matching string {s}")
+        self.low = int(groups[0][0])
+        self.hi = int(groups[0][1])
+        self.letter = groups[0][2]
 
-  def validate_count(self, s:str) -> bool:
-    times = s.count(self.letter)
-    return self.low <= times <= self.hi
+    def validate_count(self, s: str) -> bool:
+        times = s.count(self.letter)
+        return self.low <= times <= self.hi
 
-  def validate_position(self, s:str) -> bool:
-    def _xor(a, b):
-      return (a and not b) or (b and not a)
+    def validate_position(self, s: str) -> bool:
+        def _xor(a, b):
+            return (a and not b) or (b and not a)
 
-    return _xor(self.letter==s[self.low-1], self.letter==s[self.hi-1])
+        return _xor(self.letter == s[self.low-1], self.letter == s[self.hi-1])
+
 
 valid = []
 invalid = []
 
 for line in lines:
-  rule = Rule(line[0].strip())
-  pw = line[1].strip()
+    rule = Rule(line[0].strip())
+    pw = line[1].strip()
 
-  if rule.validate_count(pw):
-    valid.append(line)
-  else:
-    invalid.append(line)
+    if rule.validate_count(pw):
+        valid.append(line)
+    else:
+        invalid.append(line)
 
 print("Part 1", end='\n\t')
 print(len(valid), "valid passwords", end='\n\t')
@@ -41,13 +43,13 @@ print(len(invalid), "invalid passwords")
 p2_valid = []
 p2_invalid = []
 for line in lines:
-  rule = Rule(line[0].strip())
-  pw = line[1].strip()
+    rule = Rule(line[0].strip())
+    pw = line[1].strip()
 
-  if rule.validate_position(pw):
-    p2_valid.append(line)
-  else:
-    p2_invalid.append(line)
+    if rule.validate_position(pw):
+        p2_valid.append(line)
+    else:
+        p2_invalid.append(line)
 
 print("Part 2", end='\n\t')
 print(len(p2_valid), "valid passwords", end='\n\t')
