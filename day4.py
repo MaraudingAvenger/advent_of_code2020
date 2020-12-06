ceb06be0a1142374ec7ed0fad2168d989aa86a6a
Luke <lechambers@gmail.com>
2020-12-06 13:49:11 -0500

    Added type hints and comments

--- day4.py
+++ day4.py
@@ -1,16 +1,17 @@
 import re
 
-ds = [dict(g.split(':') for g in l.split(','))for l in [re.sub(
+dicts = [dict(g.split(':') for g in l.split(','))for l in [re.sub(
     '\s+', ',', s.strip())for s in open('day4.txt').read().split('\n\n')]]
 
-ks = ['byr', 'eyr', 'iyr', 'hgt', 'hcl', 'ecl', 'pid']
+keys = ['byr', 'eyr', 'iyr', 'hgt', 'hcl', 'ecl', 'pid']
 
-valid = [d for d in ds if all(k in d for k in ks)]
+valid = [d for d in dicts if all(k in d for k in keys)]
 
 print("Part 1:", len(valid), "valid passports")
 
 
-def hgt(x):
+def hgt(x) -> bool:
+    '''height validator func'''
     if not any(l.isalpha() for l in x):
         return False
     patt = re.compile("([0-9]{2,3})(cm|in)")
@@ -27,13 +28,13 @@ def hgt(x):
 
 new_valid = [
     d for d in valid
-    if 1920 <= int(d['byr']) <= 2002 and
-    2010 <= int(d['iyr']) <= 2020 and
-    2020 <= int(d['eyr']) <= 2030 and
-    hgt(d['hgt']) and
-    re.search('#[0-9a-f]{6}', d['hcl']) and
-    d['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] and
-    all(x.isdigit() for x in d['pid']) and len(d['pid']) == 9
+    if 1920 <= int(d['byr']) <= 2002 and # birth year validator
+    2010 <= int(d['iyr']) <= 2020 and # issue year validator
+    2020 <= int(d['eyr']) <= 2030 and # exp year validator
+    hgt(d['hgt']) and # height validator
+    re.search('#[0-9a-f]{6}', d['hcl']) and # hair clr validator
+    d['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] and # eye clr validator
+    all(x.isdigit() for x in d['pid']) and len(d['pid']) == 9 # pid validator
 ]
 
 print("part 2:", len(new_valid), 'valid passports')
